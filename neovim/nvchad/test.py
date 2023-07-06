7b866b10b57c8865adf87cac9ed792f81b802f94
Anish Tiwari <anishtiwari425@gmail.com>
2023-07-06 14:29:14 +0700

    Nvchad initial upload
    Current nvchad profile based on dreamsofcode youtube tutorial. LSP is functional for python but debugger is does not work due to treesitter issues.

--- neovim/nvchad/test.py
+++ neovim/nvchad/test.py
@@ -1,1 +1,15 @@
+def primes(n: int):
+    """Returns a list of the first n primes"""
+    sieve = [True] * n
+    res = []
+    for i in range(2, n):
+        if sieve[i]:
+            res.append(i)
+            for j in range(i * i, n, i):
+                sieve[j] = False
+    return False
 
+
+xs = primes(100)
+
+print(xs)
