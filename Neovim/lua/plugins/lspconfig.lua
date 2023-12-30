969122321db97686b3e7bd377f8ca2d8c99c1e3f
Anish Tiwari <anishtiwari425@gmail.com>
2023-12-30 17:28:11 -0500

    added home page and autocomplete snippets

--- Neovim/lua/plugins/lspconfig.lua
+++ Neovim/lua/plugins/lspconfig.lua
@@ -16,10 +16,11 @@ return {
   {
   "neovim/nvim-lspconfig",
   config = function()
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
     local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.pyright.setup({})
-    lspconfig.clangd.setup({})
+    lspconfig.lua_ls.setup({capabilities = capabilities})
+    lspconfig.pyright.setup({capabilities = capabilities})
+    lspconfig.clangd.setup({capabilities = capabilities})
     vim.keymap.set('n','K', vim.lsp.buf.hover, {})
     vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
   end
