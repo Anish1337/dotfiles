b524c1d040a3578caf99e329ff7e2596693ef63e
Anish Tiwari <anishtiwari425@gmail.com>
2025-11-14 11:45:35 -0500

    init upload
    from omarchy

---
+++ nvim2/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.relativenumber = false
+vim.cmd("set expandtab")
+-- tabs (4 spaces ofc)
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
